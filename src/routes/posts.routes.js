0aadda239bf7b5fee91606d017ef9d7043d9f624
jose fernando <jofer0628@gmail.com>
2022-04-26 12:32:12 -0500

    TODO routes

--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -8,8 +8,18 @@ const postRoutes = (app) => {
   app.use("/api/v1/posts", router);
 
   router.get("/", listPosts);
+
+  // TODO: Get by ID route
+  // router.get("/:id");
+
   // TODO: Post route
-  // ?router.post("/");
+  // router.post("/");
+
+  // TODO: Put by ID route
+  // router.put("/:id");
+
+  // TODO: Delete by ID route
+  // router.delete("/:id");
 };
 
 module.exports = postRoutes;
